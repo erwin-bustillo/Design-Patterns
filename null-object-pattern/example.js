d033194c20c5e2a316bae7d874d9d26b245061c1
Erwin Bustillo <erwin.bustillo@globant.com>
2020-09-01 15:23:28 -0500

    CHANGELOG: 1. Added null object pattern example

---
+++ null-object-pattern/example.js
@@ -0,0 +1,40 @@
+class User {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+  hasAccess() { return this.name === "Bob"; }
+  }
+  const users = [ new User(1, "Bob"), new User(2, "John")]
+
+  class NullUser {
+    constructor() {
+     this.id= -1;
+     this.name= "Guest"
+    }
+   hasAccess() {
+     return false;
+    }
+   }
+
+   function getUser(id){
+     const user = users.find(user => user.id === id);
+     if (user == null) {
+       return new NullUser();
+     } else {
+       return user;
+     }
+   }
+
+   function printUser(id){
+     const user = getUser(id);
+     console.log("Hello "+ user.name);
+
+     if (user.hasAccess()) {
+       console.log("You have access..")
+     } else {
+       console.log("you are not allowed here")
+     }
+   }
+
+   printUser(3);
